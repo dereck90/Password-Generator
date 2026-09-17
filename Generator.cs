1e8035e06bc1a007269aa8bc390a659cafb02d0f
Dereck <derecktalavera@gmail.com>
2026-09-17 18:33:24 -0400

    Bug fixes in Generator.cs

--- Generator.cs
+++ Generator.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System;
 
 namespace Password_Generator
@@ -18,21 +19,30 @@ namespace Password_Generator
             string numbers = "0123456789";
             string symbols = "!@#$%^&*()_+-=[]{}|;:,./<>?\\'" + '"';
 
-            //Creates an instance of the random class
-            Random rand = new Random();
+            //Uses a cryptographically secure random number generator instead of
+            //System.Random, which is predictable and not appropriate for generating
+            //passwords.
+            using RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            int NextInt(int maxExclusive)
+            {
+                byte[] buffer = new byte[4];
+                rng.GetBytes(buffer);
+                uint value = BitConverter.ToUInt32(buffer, 0);
+                return (int)(value % (uint)maxExclusive);
+            }
 
             //Combines all the different characters into a single string
             string combination = string.Concat(caps + lower + numbers + symbols);
 
             //Ensures that the password contains one randomly selected character from each character set
-            string randomPass = string.Concat(caps[rand.Next(caps.Length)]);
-            randomPass += string.Concat(lower[rand.Next(lower.Length)]);
-            randomPass += string.Concat(numbers[rand.Next(numbers.Length)]);
-            randomPass += string.Concat(symbols[rand.Next(symbols.Length)]);
+            string randomPass = string.Concat(caps[NextInt(caps.Length)]);
+            randomPass += string.Concat(lower[NextInt(lower.Length)]);
+            randomPass += string.Concat(numbers[NextInt(numbers.Length)]);
+            randomPass += string.Concat(symbols[NextInt(symbols.Length)]);
 
             //Generates the rest of the remaining characters of the password
             randomPass += string.Concat(Enumerable.Repeat(combination, length - 4)
-              .Select(s => s[rand.Next(s.Length)]).ToArray());
+              .Select(s => s[NextInt(s.Length)]).ToArray());
 
             //Shuffles the characters on the password
             randomPass = new string(randomPass.ToCharArray().OrderBy(x => Guid.NewGuid()).ToArray());
@@ -47,10 +57,10 @@ namespace Password_Generator
         }
         private void btnCopy_Click(object sender, EventArgs e)
         {
-            if ((tbPassword.Text == "Copied") || (tbPassword.Text == ""))
+            if ((tbPassword.Text == "Copied!") || (tbPassword.Text == ""))
             {
                 tbPassword.Text = "";
-                MessageBox.Show("¡Please generate a new password!");
+                MessageBox.Show("Â¡Please generate a new password!");
             }
             else
             {
